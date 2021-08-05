19e1a55dda3a381958108d965992418c4984641b
Samiur Prapon <samiur.prapon@gmail.com>
2021-08-06 00:54:39 +0600

    Server: Refresh endpoint issue solved.

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -5,19 +5,31 @@ const config = require('../config/databaseSecrets.json');
 
 // check refresh token is valid or not
 const authentication = (req, res, next) => {
+
+  if(req.headers["authorization"] == null){
+    return res.status(401).send({
+      success: false,
+      message: "Login first!",
+    });
+  }
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
   if (token == null) {
-    return res.status(401).send();
+    return res.status(401).send({
+      success: false,
+      message: "jwt malformed!",
+    });
   }
-
 
   jwt.verify(token, process.env.REFRESH_TOKEN_SECRET || config.REFRESH_TOKEN_SECRET , (err, user) => {
 
     if (err) {
-      console.log(err);
-      return res.status(403).send();
+      // console.log(err);
+      return res.status(403).send({
+        success: false,
+        message: err.message,
+      });
     }
 
     res.locals.user = user;
@@ -34,12 +46,18 @@ const validation = (req, res, next) => {
   const token = authHeader && authHeader.split(" ")[1];
 
   if (token == null) {
-    return res.status(401).send();
+    return res.status(401).send({
+      "success": false,
+      "message": "Unauthorized access!",
+    });
   }
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET || config.ACCESS_TOKEN_SECRET, (err, details) => { 
     if(err) {
-      return res.status(403).send();
+      return res.status(403).send({
+        success: false,
+        message: "Unauthorized access!",
+      });
     }
 
     res.locals.user = details;
