bedb01ca701735b95d0a64ed2372b86c5f70ddd4
Anik Sarker <sarkerjr@pop-os.localdomain>
2022-10-06 12:32:23 +0600

    Frontend: login page implemented and homepage edited

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react';
 import './App.css';
 import Index from './pages/global';
+import Login from './pages/global/login';
 
 function App() {
 	return (
 		<div>
-			<Index />
+			<Login />
 		</div>
 	);
 }
