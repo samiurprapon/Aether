27c5c7e2226a87b829ccf374e8f5fda982b086a5
Anik Sarker <sarkerjr@pop-os.localdomain>
2022-10-08 00:40:47 +0600

    Frontend: react redux and react router dom added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+
 import './index.css'
+import store from './store/store';
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+		<HashRouter>
+			<App />
+		</HashRouter>
+	</Provider>
 )
---
+++ client/src/utils/helper/pathHelper.js
@@ -0,0 +1,9 @@
+/* Global path constant management */
+
+const BASE_PATH = ''
+
+export const INDEX_PATH = BASE_PATH + '/'
+
+/* Auth Paths */
+export const LOGIN_PATH = BASE_PATH + '/login'
+export const REGISTER_PATH = BASE_PATH + '/register'
