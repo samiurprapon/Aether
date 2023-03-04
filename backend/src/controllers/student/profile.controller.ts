ce1df96131b627f3fb051f0969b0f60caba5fff9
Samiur Prapon <samiur.prapon@gmail.com>
2023-03-04 15:45:01 +0600

    backend: Implement student profile apis

--- backend/src/controllers/student/profile.controller.ts
+++ backend/src/controllers/student/profile.controller.ts
@@ -1,13 +1,89 @@
 import { Request, Response } from 'express';
+import { PrismaClient, Prisma } from '@prisma/client';
 
 export async function getProfile(req: Request, res: Response) {
-	return res.status(200).json({
-		message: 'Student Profile fetched successfully!',
-	});
+	const data = res.locals.data;
+
+	const student: Prisma.StudentsWhereUniqueInput = {
+		id: data.details.id,
+	};
+
+	const prisma = new PrismaClient();
+
+	return await prisma.students
+		.findUnique({
+			where: student,
+			select: {
+				id: true,
+				studentID: true,
+				Users: {
+					select: {
+						id: true,
+						name: true,
+						email: true,
+						school: true,
+					},
+				},
+			},
+		})
+		.then(profile => {
+			return res.status(200).json({
+				message: 'Student Profile fetched successfully!',
+				profile,
+			});
+		})
+		.catch(err => {
+			console.log(err);
+
+			return res.status(500).json({
+				message: 'Something went wrong!',
+			});
+		})
+		.finally(async () => {
+			await prisma.$disconnect();
+		});
 }
 
 export async function updateProfile(req: Request, res: Response) {
-	return res.status(200).json({
-		message: 'Student Profile updated successfully!',
-	});
+	const data = res.locals.data;
+
+	const { name, school, studentId, sex }: { name: string; school: string; studentId: string; sex: 'MALE' | 'FEMALE' | 'OTHER' } = req.body;
+
+	const student: Prisma.StudentsWhereUniqueInput = {
+		id: data.details.id,
+	};
+
+	const newStudent: Prisma.StudentsUpdateInput = {
+		studentID: studentId,
+
+		Users: {
+			update: {
+				name,
+				school,
+				sex,
+			},
+		},
+	};
+
+	const prisma = new PrismaClient();
+
+	return await prisma.students
+		.update({
+			where: student,
+			data: newStudent,
+		})
+		.then(() => {
+			return res.status(200).json({
+				message: 'Profile updated successfully!',
+			});
+		})
+		.catch(err => {
+			return res.status(500).json({
+				message: 'Profile updation failed!',
+				error: err,
+			});
+		})
+		.finally(async () => {
+			await prisma.$disconnect();
+		});
 }
