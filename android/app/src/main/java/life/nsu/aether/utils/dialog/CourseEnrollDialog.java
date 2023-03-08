1fafe51ba962d41cb09023efa81c72bb4011678c
mahfuj-ahmed-jim <mahfujahmedjim32@gmail.com>
2023-03-08 11:53:20 +0600

    Enroll Course Dialog

---
+++ android/app/src/main/java/life/nsu/aether/utils/dialog/CourseEnrollDialog.java
@@ -0,0 +1,39 @@
+/*
+ * CourseEnrollDialog Created by Mahfuj Ahmed Jim
+ * Last modified  3/8/23, 11:44 AM
+ * Copyright (c) 2023. All rights reserved.
+ *
+ */
+
+package life.nsu.aether.utils.dialog;
+
+import android.app.Activity;
+import android.app.Dialog;
+
+import life.nsu.aether.R;
+
+public class CourseEnrollDialog {
+
+    private Activity activity;
+    private Dialog dialog;
+
+    public CourseEnrollDialog(Activity activity) {
+        this.activity = activity;
+        setUpDialog();
+    }
+
+    public void show(){
+        dialog.show();
+    }
+
+    public void dismiss(){
+        dialog.dismiss();
+    }
+
+    private void setUpDialog() {
+        dialog = new Dialog(activity);
+        dialog.setContentView(R.layout.dialog_enroll_course);
+        dialog.setCancelable(false);
+    }
+
+}
