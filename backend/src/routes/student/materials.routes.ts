242d1c16ebf456a3975c2c691c0725aa2ed9aff0
Samiur Prapon <samiur.prapon@gmail.com>
2023-03-08 23:11:09 +0600

    backend: implement course matrial routes for students and teachers

--- backend/src/routes/student/materials.routes.ts
+++ backend/src/routes/student/materials.routes.ts
@@ -2,10 +2,8 @@ import { Router } from 'express';
 
 import { read } from '../../controllers/student/materials.controller';
 
-import { fileUpload } from '../../middlewares/file.middleware';
-
 const router = Router();
 
-router.get('/:courseId' read);
+router.get('/:courseId', read);
 
 export default router;
