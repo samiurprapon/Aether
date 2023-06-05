0a4c37e5edfd65379484ccaf36f7dbd6643defa2
Mr. M1M3 <abir.sheikh.4044@gmail.com>
2023-06-05 21:14:47 +0600

    :heavy_plus_sign: Basic login page setup

--- frontend/src/routes/PublicRoutes.tsx
+++ frontend/src/routes/PublicRoutes.tsx
@@ -6,6 +6,7 @@ import MinimalLayout from '../layouts/MinimalLayout';
 import GuestGuard from '../utils/guards/GuestGuard';
 
 const Landing = Loadable(lazy(() => import('../pages/landng')));
+const Login = Loadable(lazy(() => import('../pages/login')));
 
 const PublicRoutes = {
 	path: '/',
@@ -21,6 +22,10 @@ const PublicRoutes = {
 			path: '/',
 			element: <Landing />,
 		},
+		{
+			path: '/login',
+			element: <Login/>
+		}
 	],
 };
 
