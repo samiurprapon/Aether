224dd9488e501d5242d7f5ffac85331ae64f7dc3
Samiur Prapon <samiur.prapon@gmail.com>
2024-09-23 01:25:45 +0600

    chore: 🔥 Move all legacy code to the museum branch

--- legacy/client/src/utils/helper/pathHelper.js
+++
@@ -1,9 +0,0 @@
-/* Global path constant management */
-
-const BASE_PATH = ''
-
-export const INDEX_PATH = BASE_PATH + '/'
-
-/* Auth Paths */
-export const LOGIN_PATH = BASE_PATH + '/login'
-export const SIGNUP_PATH = BASE_PATH + '/signup'
--- legacy/server/controllers/support/studentController.js
+++
@@ -1,1 +0,0 @@
-const Student = require('../models/students');
